9767c10487dd48b578278782c78a87321ab27ba4
Dimas Sudjito <almuhtad@ualberta.ca>
2021-11-12 15:44:47 -0700

    get dictionary with required data

---
+++ xml_to_json.py
@@ -0,0 +1,77 @@
+from bs4 import BeautifulSoup
+NUM_FILES = 3999
+INSTRUCTIONS = ["pneumonia", "edema", "focal consolidation", "pleural effusion", "pneumothorax"]
+
+"""
+Convert medical reports in xml file into json consisting of
+image file, target sentence, masked paragraph, and instructions
+(keyword).
+"""
+
+# Clear output file and prepare output dictionary
+outfile = open("captions.txt", "w")
+output_dict = dict()
+
+# to keep track number of each instruction
+instruction_counter = dict()
+for instruction in INSTRUCTIONS:
+    instruction_counter[instruction] = 0
+
+# Read the XML file
+for i in range (1, NUM_FILES+1):
+    temp_dict = dict()
+
+    try:
+        with open("NLMCXR_reports/ecgen-radiology/"+str(i)+".xml", "r") as infile:
+            contents = infile.read()
+            soup = BeautifulSoup(contents,"xml")
+
+            # get caption ---------------------------
+            finding = soup.find('AbstractText', Label="FINDINGS").get_text().lower()
+            
+            if not finding:
+                continue # eliminate entries with empty findings
+            
+            contains_instruction = False
+            for instruction in INSTRUCTIONS:
+                if instruction in finding:
+                    contains_instruction = True
+                    temp_dict["instruction"] = instruction
+                    break # get the first instruction
+            if not contains_instruction:
+                continue
+             
+            finding = finding.split(".")
+            target = ""
+            for j in range(len(finding)):
+                if temp_dict["instruction"] in finding[j]:
+                    target = finding[j]
+                    finding[j] = "<fill-in-the-blank>"
+                    break # get the first sentence mentioning the instruction
+            temp_dict["target"] = target
+
+            # reconstruct masked sentence
+            finding.pop() # remove the empty string
+            masked = ""
+            for sentence in finding:
+                masked += sentence + " . "
+            temp_dict["masked"] = masked
+            
+            # get image ----------------------------
+            image = soup.find('url').get_text()
+            if not image:
+                continue # eliminate entries with empty images
+            image = image.replace("/hadoop/storage/radiology/extract/", "") # remove url
+            image = image.replace("jpg", "png") # change filetype
+            temp_dict["image"] = image
+
+            # add to output dictionary
+            output_dict[i] = temp_dict
+            instruction_counter[temp_dict["instruction"]] += 1
+    except:
+        pass
+
+# Close output file
+print("length: " + str(len(output_dict)))
+print(instruction_counter)
+outfile.close()
